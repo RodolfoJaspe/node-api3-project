6d7b274b6a4c4f453bc25b9d7061f7e756b9d6f4
RodolfoJaspe <macosi@Rodolfos-iMac.local>
2021-09-07 23:11:05 -0400

    all tests passing

--- api/server.js
+++ api/server.js
@@ -4,9 +4,17 @@ const server = express();
 
 // remember express by default cannot parse JSON in request bodies
 
+server.use(express.json())
+
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
+const {logger} = require("./middleware/middleware")
+
+const userRouter = require("./users/users-router")
+
+server.use("/api/users", logger, userRouter)
+
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
